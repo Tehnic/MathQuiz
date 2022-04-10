edf947b7b28c006b26361fa8809d7e6e4b896eb7
Tehnic <lemursltv@gmail.com>
2022-04-10 22:04:10 +0300

    Layout sketch.

---
+++ app/src/main/java/com/example/mathquiz/QuizTime.java
@@ -0,0 +1,14 @@
+package com.example.mathquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_time);
+    }
+}
